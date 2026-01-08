c8b12f5864c65955285927fb774df5cab4ffc490
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-08 12:27:29 +0000

    Load thorvg library, and test that it initialises

---
+++ internal/lib.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/ebitengine/purego"
+)
+
+//go:embed lib/libthorvg-1.so.1.0.0
+var sharedObject []byte
+
+func libraryFilepath() (string, error) {
+	filepath := filepath.Join(os.TempDir(), fmt.Sprintf("libthorvg-%s.so", sharedObjectHash))
+
+	err := os.WriteFile(filepath, sharedObject, 0755)
+	if err != nil {
+		return "", fmt.Errorf("failed to write shared object file %q : %w", filepath, err)
+	}
+
+	return filepath, nil
+}
+
+var tvg_engine_init func(threads int) Tvg_Result
+var tvg_swcanvas_create func(op int) Tvg_Canvas
+
+func Init() error {
+	filepath, err := libraryFilepath()
+	if err != nil {
+		return err
+	}
+
+	lib, err := purego.Dlopen(filepath, purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	if err != nil {
+		panic(err)
+	}
+
+	purego.RegisterLibFunc(&tvg_engine_init, lib, "tvg_engine_init")
+	purego.RegisterLibFunc(&tvg_swcanvas_create, lib, "tvg_swcanvas_create")
+
+	return nil
+}
---
+++ internal/lib_test.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	assert.NoError(t, Init())
+
+	result := tvg_engine_init(4)
+	assert.Equal(t, TVG_RESULT_SUCCESS, result)
+	canvas := tvg_swcanvas_create(1)
+	assert.NotZero(t, canvas)
+}
