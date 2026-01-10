042f707dd66631efd7b5e3969b088c62668b85ea
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-10 12:06:56 +0000

    Simplify common example drawing

--- example/draw/draw.go
+++ example/draw/draw.go
@@ -8,49 +8,35 @@ import (
 //
 // It is used by multiple examples, all of which should render identically.
 func SimpleShapes(canvas tvg.Canvas, width float32, height float32) error {
-	var err error
-
 	// background
-	{
-		rect := tvg.ShapeNew()
-		err = rect.AppendRect(0, 0, width, height, 0, 0, true)
-		if err != nil {
-			return err
-		}
-		err = rect.SetFillColor(255, 255, 255, 255)
-		if err != nil {
-			return err
-		}
-		err = canvas.Push(rect)
-		if err != nil {
-			return err
-		}
+	bg := tvg.ShapeNew()
+	if err := bg.AppendRect(0, 0, width, height, 0, 0, true); err != nil {
+		return err
+	}
+	if err := bg.SetFillColor(255, 255, 255, 255); err != nil {
+		return err
+	}
+	if err := canvas.Push(bg); err != nil {
+		return err
 	}
 
 	// foreground
-	{
-		rect := tvg.ShapeNew()
-		err = rect.AppendRect(50, 50, 200, 200, 20, 20, true)
-		if err != nil {
-			return err
-		}
-		err = rect.SetFillColor(255, 0, 0, 100)
-		if err != nil {
-			return err
-		}
-		err = canvas.Push(rect)
-		if err != nil {
-			return err
-		}
+	rect := tvg.ShapeNew()
+	if err := rect.AppendRect(50, 50, 200, 200, 20, 20, true); err != nil {
+		return err
+	}
+	if err := rect.SetFillColor(255, 0, 0, 100); err != nil {
+		return err
+	}
+	if err := canvas.Push(rect); err != nil {
+		return err
 	}
 
 	// finish
-	err = canvas.Draw(true)
-	if err != nil {
+	if err := canvas.Draw(true); err != nil {
 		return err
 	}
-	err = canvas.Sync()
-	if err != nil {
+	if err := canvas.Sync(); err != nil {
 		return err
 	}
 
