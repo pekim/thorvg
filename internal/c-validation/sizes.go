16ac8a6ecfb137204950110cadcabb11fcc7e6ae
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-19 18:00:15 +0000

    Use C types and values for all enums

--- internal/c-validation/sizes.go
+++ internal/c-validation/sizes.go
@@ -9,19 +9,6 @@ import (
 	tvg "github.com/pekim/thorvg"
 )
 
-var result C.Tvg_Result
-var colorspace C.Tvg_Colorspace
-var engineOption C.Tvg_Engine_Option
-var maskMethod C.Tvg_Mask_Method
-var blendMethod C.Tvg_Blend_Method
-var type_ C.Tvg_Type
-var pathCommand C.Tvg_Path_Command
-var strokeCap C.Tvg_Stroke_Cap
-var strokeJoin C.Tvg_Stroke_Join
-var strokeFill C.Tvg_Stroke_Fill
-var fillRule C.Tvg_Fill_Rule
-var textWrap C.Tvg_Text_Wrap
-
 var sizes = []struct {
 	name string
 	c    int
@@ -31,18 +18,4 @@ var sizes = []struct {
 	{"ColorStop", C.sizeof_Tvg_Color_Stop, unsafe.Sizeof(tvg.ColorStop{})},
 	{"Point", C.sizeof_Tvg_Point, unsafe.Sizeof(tvg.Point{})},
 	{"Matrix", C.sizeof_Tvg_Matrix, unsafe.Sizeof(tvg.Matrix{})},
-
-	// enum types
-	{"Result", int(unsafe.Sizeof(result)), unsafe.Sizeof(tvg.RESULT_SUCCESS)},
-	{"ColorSpace", int(unsafe.Sizeof(colorspace)), unsafe.Sizeof(tvg.COLORSPACE_ABGR8888)},
-	{"EngineOption", int(unsafe.Sizeof(engineOption)), unsafe.Sizeof(tvg.ENGINE_OPTION_NONE)},
-	{"MaskMethod", int(unsafe.Sizeof(maskMethod)), unsafe.Sizeof(tvg.MASK_METHOD_NONE)},
-	{"BlendMethod", int(unsafe.Sizeof(blendMethod)), unsafe.Sizeof(tvg.BLEND_METHOD_NORMAL)},
-	{"Type", int(unsafe.Sizeof(type_)), unsafe.Sizeof(tvg.TYPE_UNDEF)},
-	{"PathCommand", int(unsafe.Sizeof(pathCommand)), unsafe.Sizeof(tvg.PATH_COMMAND_CLOSE)},
-	{"StrokeCap", int(unsafe.Sizeof(strokeCap)), unsafe.Sizeof(tvg.STROKE_CAP_BUTT)},
-	{"StrokeJoin", int(unsafe.Sizeof(strokeJoin)), unsafe.Sizeof(tvg.STROKE_JOIN_MITER)},
-	{"StrokeFill", int(unsafe.Sizeof(strokeFill)), unsafe.Sizeof(tvg.STROKE_FILL_PAD)},
-	{"FillRule", int(unsafe.Sizeof(fillRule)), unsafe.Sizeof(tvg.FILL_RULE_NON_ZERO)},
-	{"TextWrap", int(unsafe.Sizeof(textWrap)), unsafe.Sizeof(tvg.TEXT_WRAP_NONE)},
 }
