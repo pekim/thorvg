cdeb37b322d8005b1116f5619d183afa45d9f0e6
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-21 13:04:40 +0000

    Convert Saver and its methods to using cgo

--- api-saver.go
+++ api-saver.go
@@ -1,96 +1,113 @@
 package thorvg
 
-// type Saver uintptr
+// #include "thorvg_capi.h"
+// #include <stdlib.h>
+import "C"
 
-// /*
-// SaverNew Creates a new Tvg_Saver object.
+import "unsafe"
 
-// 	@return A new Tvg_Saver object.
-// */
-// func SaverNew() Saver {
-// 	return Saver(tvg_saver_new())
-// }
+type Saver struct {
+	saver C.Tvg_Saver
+}
 
-// /*
-// SavePaint exports the given @p paint data to the given @p path
+/*
+SaverNew Creates a new Tvg_Saver object.
 
-// If the saver module supports any compression mechanism, it will optimize the data size.
-// This might affect the encoding/decoding time in some cases. You can turn off the compression
-// if you wish to optimize for speed.
+	@return A new Tvg_Saver object.
+*/
+func SaverNew() Saver {
+	return Saver{C.tvg_saver_new()}
+}
 
-// 	@param[in] saver The Tvg_Saver object connected with the saving task.
-// 	@param[in] paint The paint to be saved with all its associated properties.
-// 	@param[in] path A path to the file, in which the paint data is to be saved.
-// 	@param[in] quality The encoded quality level. @c 0 is the minimum, @c 100 is the maximum value(recommended).
+/*
+SavePaint exports the given @p paint data to the given @p path
 
-// 	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
-// 	@retval TVG_RESULT_INSUFFICIENT_CONDITION Currently saving other resources.
-// 	@retval TVG_RESULT_NOT_SUPPORTED Trying to save a file with an unknown extension or in an unsupported format.
-// 	@retval TVG_RESULT_UNKNOWN An empty paint is to be saved.
+If the saver module supports any compression mechanism, it will optimize the data size.
+This might affect the encoding/decoding time in some cases. You can turn off the compression
+if you wish to optimize for speed.
 
-// 	@note Saving can be asynchronous if the assigned thread number is greater than zero. To guarantee the saving is done, call tvg_saver_sync() afterwards.
-// 	@see tvg_saver_sync()
-// */
-// func (saver Saver) SavePaint(paint Paint, path string, quality uint) error {
-// 	return tvg_saver_save_paint(uintptr(saver), paint.paint(), path, uint32(quality)).error()
-// }
+	@param[in] saver The Tvg_Saver object connected with the saving task.
+	@param[in] paint The paint to be saved with all its associated properties.
+	@param[in] path A path to the file, in which the paint data is to be saved.
+	@param[in] quality The encoded quality level. @c 0 is the minimum, @c 100 is the maximum value(recommended).
 
-// /*
-// SaveAnimation Exports the given @p animation data to the given @p path
+	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
+	@retval TVG_RESULT_INSUFFICIENT_CONDITION Currently saving other resources.
+	@retval TVG_RESULT_NOT_SUPPORTED Trying to save a file with an unknown extension or in an unsupported format.
+	@retval TVG_RESULT_UNKNOWN An empty paint is to be saved.
 
-// If the saver module supports any compression mechanism, it will optimize the data size.
-// This might affect the encoding/decoding time in some cases. You can turn off the compression
-// if you wish to optimize for speed.
+	@note Saving can be asynchronous if the assigned thread number is greater than zero. To guarantee the saving is done, call tvg_saver_sync() afterwards.
+	@see tvg_saver_sync()
+*/
+func (saver Saver) SavePaint(paint Paint, path string, quality uint) error {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
 
-// 	@param[in] saver The Tvg_Saver object connected with the saving task.
-// 	@param[in] animation The animation to be saved with all its associated properties.
-// 	@param[in] path A path to the file, in which the animation data is to be saved.
-// 	@param[in] quality The encoded quality level. @c 0 is the minimum, @c 100 is the maximum value(recommended).
-// 	@param[in] fps The frames per second for the animation. If @c 0, the default fps is used.
+	result := C.tvg_saver_save_paint(saver.saver, paint.paint(), cPath, C.uint32_t(quality))
+	return resultError(result)
+}
 
-// 	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
-// 	@retval TVG_RESULT_INSUFFICIENT_CONDITION Currently saving other resources or animation has no frames.
-// 	@retval TVG_RESULT_NOT_SUPPORTED Trying to save a file with an unknown extension or in an unsupported format.
-// 	@retval TVG_RESULT_UNKNOWN Unknown if attempting to save an empty paint.
+/*
+SaveAnimation Exports the given @p animation data to the given @p path
 
-// 	@note A higher frames per second (FPS) would result in a larger file size. It is recommended to use the default value.
-// 	@note Saving can be asynchronous if the assigned thread number is greater than zero. To guarantee the saving is done, call tvg_saver_sync() afterwards.
+If the saver module supports any compression mechanism, it will optimize the data size.
+This might affect the encoding/decoding time in some cases. You can turn off the compression
+if you wish to optimize for speed.
 
-// 	@see tvg_saver_sync()
+	@param[in] saver The Tvg_Saver object connected with the saving task.
+	@param[in] animation The animation to be saved with all its associated properties.
+	@param[in] path A path to the file, in which the animation data is to be saved.
+	@param[in] quality The encoded quality level. @c 0 is the minimum, @c 100 is the maximum value(recommended).
+	@param[in] fps The frames per second for the animation. If @c 0, the default fps is used.
 
-// 	@since 1.0
-// */
-// func (saver Saver) SaveAnimation(animation Animation, path string, quality uint, fps uint) error {
-// 	return tvg_saver_save_animation(uintptr(saver), uintptr(animation), path, uint32(quality), uint32(fps)).error()
-// }
+	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
+	@retval TVG_RESULT_INSUFFICIENT_CONDITION Currently saving other resources or animation has no frames.
+	@retval TVG_RESULT_NOT_SUPPORTED Trying to save a file with an unknown extension or in an unsupported format.
+	@retval TVG_RESULT_UNKNOWN Unknown if attempting to save an empty paint.
 
-// /*
-// Sync guarantees that the saving task is finished.
+	@note A higher frames per second (FPS) would result in a larger file size. It is recommended to use the default value.
+	@note Saving can be asynchronous if the assigned thread number is greater than zero. To guarantee the saving is done, call tvg_saver_sync() afterwards.
 
-// The behavior of the Saver module works on a sync/async basis, depending on the threading setting of the Initializer.
-// Thus, if you wish to have a benefit of it, you must call tvg_saver_sync() after the tvg_saver_save_paint() in the proper delayed time.
-// Otherwise, you can call tvg_saver_sync() immediately.
+	@see tvg_saver_sync()
 
-// 	@param[in] saver The Tvg_Saver object connected with the saving task.
+	@since 1.0
+*/
+func (saver Saver) SaveAnimation(animation Animation, path string, quality uint, fps uint) error {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
 
-// 	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
-// 	@retval TVG_RESULT_INSUFFICIENT_CONDITION No saving task is running.
+	result := C.tvg_saver_save_animation(saver.saver, animation.animation, cPath, C.uint32_t(quality), C.uint32_t(fps))
+	return resultError(result)
+}
 
-// 	@note The asynchronous tasking is dependent on the Saver module implementation.
-// 	@see tvg_saver_save_paint()
-// */
-// func (saver Saver) Sync() error {
-// 	return tvg_saver_sync(uintptr(saver)).error()
-// }
+/*
+Sync guarantees that the saving task is finished.
 
-// /*
-// Del deletes the given Tvg_Saver object.
+The behavior of the Saver module works on a sync/async basis, depending on the threading setting of the Initializer.
+Thus, if you wish to have a benefit of it, you must call tvg_saver_sync() after the tvg_saver_save_paint() in the proper delayed time.
+Otherwise, you can call tvg_saver_sync() immediately.
 
-// @param[in] saver The Tvg_Saver object to be deleted.
+	@param[in] saver The Tvg_Saver object connected with the saving task.
 
-// @retval TVG_RESULT_INVALID_ARGUMENT An invalid Tvg_Saver pointer.
-// */
-// func (saver Saver) Del() error {
-// 	return tvg_saver_del(uintptr(saver)).error()
+	@retval TVG_RESULT_INVALID_ARGUMENT A @c nullptr passed as the argument.
+	@retval TVG_RESULT_INSUFFICIENT_CONDITION No saving task is running.
 
-// }
+	@note The asynchronous tasking is dependent on the Saver module implementation.
+	@see tvg_saver_save_paint()
+*/
+func (saver Saver) Sync() error {
+	result := C.tvg_saver_sync(saver.saver)
+	return resultError(result)
+}
+
+/*
+Del deletes the given Tvg_Saver object.
+
+@param[in] saver The Tvg_Saver object to be deleted.
+
+@retval TVG_RESULT_INVALID_ARGUMENT An invalid Tvg_Saver pointer.
+*/
+func (saver Saver) Del() error {
+	result := C.tvg_saver_del(saver.saver)
+	return resultError(result)
+}
--- api-saver_test.go
+++ api-saver_test.go
@@ -1,25 +1,25 @@
 package thorvg
 
-// import (
-// 	"testing"
-// )
+import (
+	"testing"
+)
 
-// func TestSaverSavePaint(t *testing.T) {
-// 	defer testInitTerm(t)()
+func TestSaverSavePaint(t *testing.T) {
+	defer testInitTerm(t)()
 
-// 	background := ShapeNew()
-// 	_ = background.AppendRect(0, 0, 200, 150, 0, 0, true)
-// 	_ = background.SetFillColor(0xff, 0xff, 0xff, 0xff)
+	background := ShapeNew()
+	_ = background.AppendRect(0, 0, 200, 150, 0, 0, true)
+	_ = background.SetFillColor(0xff, 0xff, 0xff, 0xff)
 
-// 	rect := ShapeNew()
-// 	_ = rect.AppendRect(50, 25, 100, 100, 0, 0, true)
-// 	_ = rect.SetFillColor(0xff, 0x00, 0x00, 0xff)
+	rect := ShapeNew()
+	_ = rect.AppendRect(50, 25, 100, 100, 0, 0, true)
+	_ = rect.SetFillColor(0xff, 0x00, 0x00, 0xff)
 
-// 	scene := SceneNew()
-// 	_ = scene.Add(background)
-// 	_ = scene.Add(rect)
+	scene := SceneNew()
+	_ = scene.Add(background)
+	_ = scene.Add(rect)
 
-// 	// saver := SaverNew()
-// 	// _= saver.SavePaint(scene, "saved.tvg", 100)
-// 	// _= saver.Sync()
-// }
+	// saver := SaverNew()
+	// _= saver.SavePaint(scene, "saved.tvg", 100)
+	// _= saver.Sync()
+}
