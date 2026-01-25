8f1cc894c49d9436f2e592e44344f9e9ce9f025e
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-25 20:21:29 +0000

    Don't check the size of enums
    There is no point, as the Go enums are really just type definitions of
    the C enums.

--- sizes.go
+++ sizes.go
@@ -7,19 +7,6 @@ import (
 	"unsafe"
 )
 
-var result C.Tvg_Result
-var colorspace C.Tvg_Colorspace
-var engineOption C.Tvg_Engine_Option
-var maskMethod C.Tvg_Mask_Method
-var blendMethod C.Tvg_Blend_Method
-var type_ C.Tvg_Type
-var pathCommand C.Tvg_Path_Command
-var strokeCap C.Tvg_Stroke_Cap
-var strokeJoin C.Tvg_Stroke_Join
-var strokeFill C.Tvg_Stroke_Fill
-var fillRule C.Tvg_Fill_Rule
-var textWrap C.Tvg_Text_Wrap
-
 var sizes = []struct {
 	name string
 	c    int
@@ -29,18 +16,4 @@ var sizes = []struct {
 	{"ColorStop", C.sizeof_Tvg_Color_Stop, unsafe.Sizeof(ColorStop{})},
 	{"Point", C.sizeof_Tvg_Point, unsafe.Sizeof(Point{})},
 	{"Matrix", C.sizeof_Tvg_Matrix, unsafe.Sizeof(Matrix{})},
-
-	// enum types
-	{"Result", int(unsafe.Sizeof(result)), unsafe.Sizeof(RESULT_SUCCESS)},
-	{"ColorSpace", int(unsafe.Sizeof(colorspace)), unsafe.Sizeof(COLORSPACE_ABGR8888)},
-	{"EngineOption", int(unsafe.Sizeof(engineOption)), unsafe.Sizeof(ENGINE_OPTION_NONE)},
-	{"MaskMethod", int(unsafe.Sizeof(maskMethod)), unsafe.Sizeof(MASK_METHOD_NONE)},
-	{"BlendMethod", int(unsafe.Sizeof(blendMethod)), unsafe.Sizeof(BLEND_METHOD_NORMAL)},
-	{"Type", int(unsafe.Sizeof(type_)), unsafe.Sizeof(TYPE_UNDEF)},
-	{"PathCommand", int(unsafe.Sizeof(pathCommand)), unsafe.Sizeof(PATH_COMMAND_CLOSE)},
-	{"StrokeCap", int(unsafe.Sizeof(strokeCap)), unsafe.Sizeof(STROKE_CAP_BUTT)},
-	{"StrokeJoin", int(unsafe.Sizeof(strokeJoin)), unsafe.Sizeof(STROKE_JOIN_MITER)},
-	{"StrokeFill", int(unsafe.Sizeof(strokeFill)), unsafe.Sizeof(STROKE_FILL_PAD)},
-	{"FillRule", int(unsafe.Sizeof(fillRule)), unsafe.Sizeof(FILL_RULE_NON_ZERO)},
-	{"TextWrap", int(unsafe.Sizeof(textWrap)), unsafe.Sizeof(TEXT_WRAP_NONE)},
 }
--- sizes_test.go
+++ sizes_test.go
@@ -6,7 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStructs(t *testing.T) {
+// TestStructs asserts that the Go structs types are the same size as their C counterparts.
+//
+// It's just a crude sanity check.
+// It doesn't verify that all fields have the correct size and offset.
+func TestStructSize(t *testing.T) {
 	for _, size := range sizes {
 		t.Run(size.name, func(t *testing.T) {
 			assert.Equal(t, size.c, int(size.go_))
