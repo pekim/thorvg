e336af57b229ea48fd6adf7d3f92624e7ec080e2
Mike D Pilsbury <mike@pekim.co.uk>
2026-01-27 12:56:34 +0000

    Remove commented unused cString function

--- string.go
+++ string.go
@@ -7,14 +7,6 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// func cString(s string) (uintptr, func()) {
-// 	cStr := malloc(len(s) + 1)
-// 	cStrSlice := unsafe.Slice(cStr, len(s)+1)
-// 	copy(cStrSlice, []byte(s))
-// 	cStrSlice[len(s)] = 0
-// 	return uintptr(unsafe.Pointer(cStr)), func() { free(cStr) }
-// }
-
 // goString returns a Go string for a null terminated C string.
 //
 // The Go string is a copy of the C bytes.
